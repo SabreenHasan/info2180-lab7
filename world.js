051e2fca3fc19be82387779dcbf21eb1f503ea25
Sabreen Hasan <sabreen_hasan@yahoo.com>
2018-11-19 17:00:02 -0500

    completed exercise 4

--- world.js
+++ world.js
@@ -3,16 +3,32 @@ window.onload = startUp;
 function startUp() {
 	var httpReq;
 	var lookUpButtn = document.getElementById("lookup");
-	lookUpButtn.addEventListener('click', function runLookUp(e) {
+	var allCuntryzButtn = document.getElementById("allCountries");
+	
+    lookUpButtn.addEventListener('click', function runLookUp(e) {
 	e.preventDefault();
 	httpReq = new XMLHttpRequest();
 
 	var input = document.getElementById("country").value;
-    
-    var url = "world.php?country=" + input;
-    httpReq.onreadystatechange = load;
-    httpReq.open('GET', url);
-    httpReq.send();
+	
+    if (allCuntryzButtn.checked == false && null !== input)
+    {
+        var url = "world.php?country=" + input;
+        httpReq.onreadystatechange = load;
+        httpReq.open('GET', url);
+        httpReq.send();
+    }
+    else if (allCuntryzButtn.checked == true)
+    {
+	    var url = "world.php?all=true";
+        httpReq.onreadystatechange = load;
+        httpReq.open('GET', url);
+        httpReq.send();    
+	}
+	else if (allCuntryzButtn.checked == false && input == null)
+	{
+        alert("No value entered.");
+	}
 });
 
 function load () {
@@ -21,11 +37,11 @@ function load () {
         if (httpReq.status === 200)
         {
             var response = httpReq.responseText;
-            alert(response);
+            document.getElementById('result').innerHTML = response;
         }
         else
         {
-            alert('ERROR!');
+            alert("ERROR!");
         }
     }
 }
--- world.php
+++ world.php
@@ -5,19 +5,34 @@ $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 $country = $_GET['country'];
+$allCountry = $_GET['all'];
 
 
-if (isset($country) || !empty($country))
+if (isset($country) && !empty($country) && $country == true)
 {
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+        foreach ($results as $row) {
+            echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+        }
+    echo '</ul>';
 }
-echo '</ul>';
+
+else if (isset($allCountry) && $allCountry == true && !empty($allCountry))
+{
+   $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+        foreach ($results as $row) {
+            echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+        }
+    echo '</ul>'; 
+}
+
+else if (isset($country) == false && isset($allCountry) == false && empty($allCountry) && empty($country))
+{ 
+    echo " ";
 }
